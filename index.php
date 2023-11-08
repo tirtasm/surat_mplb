94cd0a2d1978c71d8b56dd015bd73528b4f75946
AlbabRiziq <albabriziq123@gmail.com>
2023-11-08 08:04:08 +0700

    gd

--- index.php
+++ index.php
@@ -64,16 +64,7 @@ $jumlahSurat = mysqli_num_rows($totalSurat);
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon">sjk</span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
 
-            </div>
         </div>
     </nav>
 
